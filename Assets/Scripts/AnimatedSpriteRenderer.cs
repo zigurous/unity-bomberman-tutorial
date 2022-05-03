83a32bc4d759247d2266cefc669f32d75a0cf27f
Adam Graham <adam@zigurous.com>
2022-05-03 00:54:02 -0500

    Add scene and initial scripts

---
+++ Assets/Scripts/AnimatedSpriteRenderer.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimatedSpriteRenderer : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] walkSprites;
+    public Sprite idleSprite;
+
+    public float animationTime = 0.25f;
+    private int animationFrame;
+
+    public bool idle { get; set; }
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnEnable()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(NextFrame), animationTime, animationTime);
+    }
+
+    private void NextFrame()
+    {
+        animationFrame++;
+
+        if (animationFrame >= walkSprites.Length) {
+            animationFrame = 0;
+        }
+
+        if (idle) {
+            spriteRenderer.sprite = idleSprite;
+        } else if (animationFrame >= 0 && animationFrame < walkSprites.Length) {
+            spriteRenderer.sprite = walkSprites[animationFrame];
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private new Rigidbody2D rigidbody;
+    private Vector2 direction = Vector2.down;
+    public float moveSpeed = 5f;
+
+    public AnimatedSpriteRenderer spriteRendererUp;
+    public AnimatedSpriteRenderer spriteRendererDown;
+    public AnimatedSpriteRenderer spriteRendererLeft;
+    public AnimatedSpriteRenderer spriteRendererRight;
+    private AnimatedSpriteRenderer activeSpriteRenderer;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        activeSpriteRenderer = spriteRendererDown;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            SetDirection(Vector2.up, spriteRendererUp);
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            SetDirection(Vector2.down, spriteRendererDown);
+        } else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            SetDirection(Vector2.left, spriteRendererLeft);
+        } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            SetDirection(Vector2.right, spriteRendererRight);
+        } else {
+            SetDirection(Vector2.zero, activeSpriteRenderer);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            PlaceBomb();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * moveSpeed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDirection, AnimatedSpriteRenderer spriteRenderer)
+    {
+        direction = newDirection;
+
+        spriteRendererUp.enabled = spriteRenderer == spriteRendererUp;
+        spriteRendererDown.enabled = spriteRenderer == spriteRendererDown;
+        spriteRendererLeft.enabled = spriteRenderer == spriteRendererLeft;
+        spriteRendererRight.enabled = spriteRenderer == spriteRendererRight;
+
+        activeSpriteRenderer = spriteRenderer;
+        activeSpriteRenderer.idle = direction == Vector2.zero;
+    }
+
+    private void PlaceBomb()
+    {
+    }
+
+}
