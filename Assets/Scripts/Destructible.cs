49b4dd3334d766f7c2f03694fd5b204a51f4c75f
Adam Graham <adam@zigurous.com>
2022-05-04 01:16:09 -0500

    Decrease item spawn chance

--- Assets/Scripts/Destructible.cs
+++ Assets/Scripts/Destructible.cs
@@ -5,7 +5,7 @@ public class Destructible : MonoBehaviour
     public float destructionTime = 1f;
 
     [Range(0f, 1f)]
-    public float itemSpawnChance = 0.25f;
+    public float itemSpawnChance = 0.2f;
     public GameObject[] spawnableItems;
 
     private void Start()
